8d1b5af67d4e47b6e5c9f1d683026ffeb34ff157
Casey Cowens <caseycowens.code@gmail.com>
2022-05-24 12:14:33 -0400

    created and transferred spring initializer

---
+++ practice/src/App.js
@@ -0,0 +1,16 @@
+package src.NumberGame;
+
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    {NumberGame}
+    </div>
+  );
+}
+
+export default App;
